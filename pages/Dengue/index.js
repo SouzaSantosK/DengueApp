ef45dc2d14be58f30ddd170b4f665b25ecb3ece9
Westwitt <112172527+Westwitt@users.noreply.github.com>
2024-03-25 23:10:55 -0300

    final dengue section

--- pages/Dengue/index.js
+++ pages/Dengue/index.js
@@ -1,12 +1,16 @@
 import { StatusBar } from "expo-status-bar";
-import { Image, ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, Text, View, TouchableOpacity } from "react-native";
+import { useNavigation } from '@react-navigation/native';
+
 
 import InfoHeader from "../../src/components/InfoHeader";
 
 import "../../global.css";
 import SectionDengue from "../../src/components/SectionDengue";
 
+
 export default function Dengue() {
+  const navigation = useNavigation()
   return (
     <ScrollView className="bg-whiteBG flex-1 pb-32">
         <InfoHeader path={require('../../assets/dengue.png')} title="Sobre a dengue">
@@ -30,6 +34,22 @@ export default function Dengue() {
             </View>
             <View className="mt-[15px] flex items-center">
                 <Image source={require('../../assets/prevencao.png')} className="w-[350px] h-[150px] rounded-lg"/>
+            </View>
+        </View>
+
+        <View className="w-[350px] mx-auto mt-[50px]">
+            <View className="mb-6 flex flex-row gap-3">
+                <Text className="text-blue font-semibold text-[26px]">Saiba mais</Text>
+            </View>
+            <View>
+                <Text className="text-[16px]">
+                  Além disso, outra forma de se prevenir é através da vacinação. Clique no botão abaixo e conheça sobre as vacinas disponíveis no combate à Dengue: 
+                </Text>
+            </View>
+            <View>
+              <TouchableOpacity title="Vacinas" onPress={() => navigation.navigate('Vacinas')} className="w-[100px] h-[30px] bg-blue mt-[20px] text-center text-white justify-center rounded-[5px]">
+                Vacinas
+              </TouchableOpacity>
             </View>
         </View>
     </ScrollView>
